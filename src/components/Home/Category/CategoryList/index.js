1114f9e6eb97092e242fc68a57d8fe5e68d93d6b
potnar <potnar6@users.noreply.github.com>
2020-04-02 14:25:24 +0200

    update category styling

--- src/components/Home/Category/CategoryList/index.js
+++ src/components/Home/Category/CategoryList/index.js
@@ -5,15 +5,17 @@ import "./CategoryList.scss";
 
 const CategoryList = ({ pickedCategoryId, categories, onCategoryChange }) => {
   return (
-    <div className="category-list">
-      {categories.map(category => (
-        <StickyNoteCategory
-          key={uid(category)}
-          category={category}
-          onCategoryChange={category => onCategoryChange(category)}
-          active={pickedCategoryId === category._id}
-        />
-      ))}
+    <div className="container-list">
+      <div className="category-list">
+        {categories.map(category => (
+          <StickyNoteCategory
+            key={uid(category)}
+            category={category}
+            onCategoryChange={category => onCategoryChange(category)}
+            active={pickedCategoryId === category._id}
+          />
+        ))}
+      </div>
     </div>
   );
 };

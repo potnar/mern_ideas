925c40cf8d0a16a9ecedc4bad678b4f330ff6079
potnar <potnar6@users.noreply.github.com>
2021-04-14 20:13:04 +0200

    switch class component to function component

--- src/components/shared/ScrollBar/index.js
+++ src/components/shared/ScrollBar/index.js
@@ -2,18 +2,15 @@ import React from "react";
 import Scrollbars from "react-perfect-scrollbar";
 import "react-perfect-scrollbar/dist/css/styles.css";
 
-class Scrollbar extends React.Component {
-  render() {
-    return (
-      <Scrollbars
-        className={`custom-scrollbar ${
-          this.props.className ? this.props.className : ""
-        }`}
-      >
-        {this.props.children}
-      </Scrollbars>
-    );
-  }
-}
+const Scrollbar = React.forwardRef((props, ref) => {
+  return (
+    <Scrollbars
+      className={`custom-scrollbar ${props.className ? props.className : ""}`}
+      ref={ref}
+    >
+      {props.children}
+    </Scrollbars>
+  );
+});
 
 export default Scrollbar;
